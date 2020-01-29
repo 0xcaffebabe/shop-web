7ff8f4e47ba4e3190bd590580df8d779c2331e57
MY <715711877@qq.com>
2020-01-29 14:33:09 +0800

    轮播图增加节流阀

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function () {
     var imgIndex = 0;
+    var flag = true;
     var arrow_l = this.document.querySelector('.arrow_l');
     var arrow_r = this.document.querySelector('.arrow_r');
     var focus = this.document.querySelector('.focus');
@@ -9,6 +10,7 @@ window.addEventListener('load', function () {
         arrow_r.style.display = 'block';
         // 鼠标移入取消自动播放
         clearInterval(timer);
+        timer = null;
     });
     focus.addEventListener('mouseleave', function () {
         arrow_l.style.display = 'none';
@@ -45,25 +47,33 @@ window.addEventListener('load', function () {
     // 轮播图手动翻页
     
     arrow_r.addEventListener('click', function (e) {
-        imgIndex++;
-        animate(ul, -imgIndex * focus.offsetWidth, function () {
-            if (imgIndex == ul.children.length - 1) {
-                imgIndex = 0;
-                ul.style.left = 0;
-            }
-            circleChange()
-        });
+        if (flag){
+            flag = false;
+            imgIndex++;
+            animate(ul, -imgIndex * focus.offsetWidth, function () {
+                if (imgIndex == ul.children.length - 1) {
+                    imgIndex = 0;
+                    ul.style.left = 0;
+                }
+                circleChange();
+                flag = true;
+            });
+        }
     });
     arrow_l.addEventListener('click', function (e) {
-        if (imgIndex == 0){
-            imgIndex = ul.children.length - 1;
-            ul.style.left = -imgIndex * focus.offsetWidth +'px';
+        if (flag){
+            flag = false;
+            if (imgIndex == 0){
+                imgIndex = ul.children.length - 1;
+                ul.style.left = -imgIndex * focus.offsetWidth +'px';
+            }
+            imgIndex--;
+            animate(ul, -imgIndex * focus.offsetWidth, function () {
+                
+                circleChange();
+                flag = true;
+            });
         }
-        imgIndex--;
-        animate(ul, -imgIndex * focus.offsetWidth, function () {
-            
-            circleChange();
-        });
     });
     function circleChange() {
         for (var i = 0; i < ol.children.length; i++) {
